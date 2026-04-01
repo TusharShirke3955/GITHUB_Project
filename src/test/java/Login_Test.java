03000a26865b5bb62c1df24f68f4b8589910058b
TusharShirke <tshirke325@gmail.com>
2026-04-01 15:11:04 +0530

    Intial commit

---
+++ src/test/java/Login_Test.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login_Test {
+
+    public static void main(String[] args){
+        WebDriver driver = new ChromeDriver();
+        driver.get("www.facebook.com");
+    }
+
+}
